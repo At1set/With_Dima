5cd86d83abeed71f3191bf237a47151952cb2440
At1set <iliya9874329@gmail.com>
2023-11-10 22:43:33 +0300

    Добавлен начальный проект консольного приложения на C#

---
+++ src/TestApp/FileWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public static class FileWorker
+{
+    public static string ReadFile(string filePath)
+	{
+        StringBuilder data = new ();
+
+        FileStream file = File.Open(filePath, FileMode.Open);
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            // Читаем файл построчно
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                data.AppendLine(line);
+            }
+        }
+        return data.ToString ();
+    }
+}
